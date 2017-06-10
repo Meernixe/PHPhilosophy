dda00dada6da60b78bb7c650b7518fd5a9009cb6
Lisa Saalfrank <lsaalfrank@hof-university.de>
2017-06-10 08:18:02 +0200

    Create InputInterface.php

---
+++ vendor/Http/Interfaces/InputInterface.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Phphilosophy\Http\Interfaces;
+
+/**
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2016-2017 PHPhilosophy
+ * @package     Phphilosophy
+ * @version     1.0-thales
+ */
+interface InputInterface {
+
+    /**
+     * @param   array   $get
+     *
+     * @return  self
+     */
+    public function withGet(array $get);
+
+    /**
+     * @param   array   $post
+     *
+     * @return  self
+     */
+    public function withPost(array $post);
+
+    /**
+     * @param   string  $key
+     * @param   mixed   $value
+     *
+     * @return  self
+     */
+    public function withAddedGet(string $key, $value);
+
+    /**
+     * @param   string  $key
+     * @param   mixed   $value
+     *
+     * @return  self
+     */
+    public function withAddedPost(string $key, $value);
+
+    /**
+     * @param   string  $key
+     *
+     * @return  self
+     */
+    public function withoutGet(string $key);
+
+    /**
+     * @param   string  $key
+     *
+     * @return  self
+     */
+    public function withoutPost(string $key);
+
+    /**
+     * @param   string  $name
+     * @param   mixed   $default
+     *
+     * @return  mixed
+     */
+    public function get(string $name = null, $default = null);
+
+    /**
+     * @param   string  $name
+     * @param   mixed   $default
+     *
+     * @return  mixed
+     */
+    public function post(string $name = null, $default = null);
+}
